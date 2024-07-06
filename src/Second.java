99f93860e88f7238cad5f6f5a619c66cced51a1b
Kapil-Kumar-Github <kapilk34537@gmail.com>
2024-07-06 16:52:02 +0530

    second commit

---
+++ src/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    static int a = 1 ;
+    static int b = 2 ;
+    static int c ;
+    public static void main(String [] args){
+        c = a + b ;
+        Second s = new Second() ;
+        System.out.println("c be " + c);
+
+    }
+}
